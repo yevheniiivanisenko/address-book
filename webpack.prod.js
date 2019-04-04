e339788223a71f953adb08da798052380efed417
Yevhenii Ivanisenko <yevhenii.ivanisenko@gmail.com>
2019-04-04 17:48:15 +0300

    Change uglifyjs-webpack-plugin for terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
+      new TerserJSPlugin({}),
       new OptimizeCssAssetsPlugin()
     ]
   }
